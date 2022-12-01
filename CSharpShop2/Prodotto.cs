8992368f89469ca9f66d3313ce3ab513fe19c966
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-12-01 15:34:55 +0100

    Costruzione della classe Prodotto

---
+++ CSharpShop2/Prodotto.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop2
+{
+    public class Prodotto
+    {
+        // PROPRIETA / ATTRIBUTI
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private float prezzo;
+        private int iva;
+        // STATO
+        // COSTRUTTORI
+        // Costuttore che definisco IO e che usa il concetto di OVERLOAD
+        public Prodotto(int Codice, string Nome, string Descrizione, float Prezzo, int Iva)
+        {
+            this.codice = Codice;
+            this.nome = Nome;
+            this.descrizione = Descrizione;
+            this.prezzo = Prezzo;
+            this.iva = Iva;
+        }
+        // GETTERS
+        public int GetCodice()
+        {
+            return this.codice;
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+
+        public float GetPrezzo()
+        {
+            return this.prezzo;
+        }
+
+        public int GetIva()
+        {
+            return this.iva;
+        }
+        // SETTERS
+        public void SetNome(string Nome)
+        {
+            this.nome = Nome;
+        }
+
+        public void SetDescrizione(string Descrizione)
+        {
+            this.descrizione = Descrizione;
+        }
+
+        public void SetPrezzo(int Prezzo)
+        {
+            this.prezzo = Prezzo;
+        }
+
+        public void SetIva(int Iva)
+        {
+            this.iva = Iva;
+        }
+        // METODI PUBBLICI
+        public string StampaProdotto()
+        {
+            string stringaProdotto = ""
+                
+                
+                ;
+
+            return stringaProdotto;
+        }
+        // METODI PRIVATI
+        public float CalcoloPrezzoConIva(float prezzo, int iva)
+        {
+            float prezzoConIva = (prezzo * iva) / 100;
+            prezzoConIva += prezzo;
+
+            return prezzoConIva;
+        }
+    }
+}
--- CSharpShop2/Program.cs
+++ CSharpShop2/Program.cs
@@ -1,2 +1,93 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSharpShop2;
+
+Prodotto prodotto;
+string myString = "";
+int codice;
+string nome;
+string descrizione = "";
+float prezzo = 0;
+int iva;
+float prezzoConIva;
+bool controllo;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Random codiceRandom = new Random();
+codice = codiceRandom.Next(1, 99999999);
+
+Console.Write("Inserisci il nome del prodotto: ");
+nome = Console.ReadLine();
+
+Console.Write("Inserisci una descrizione del prodotto: ");
+descrizione = Console.ReadLine();
+
+Console.Write("Inserisci il prezzo del prodotto: ");
+do
+{
+    myString = Console.ReadLine();
+    //LimitDecimalPlace(myString, 2);
+
+    if (float.TryParse(myString, out prezzo))
+    {
+        controllo = true;
+        //Console.WriteLine("Il prezzo è: " + $"{prezzo:0.##}" + "\n");
+    }
+    else
+    {
+        Console.WriteLine("\nHai inserito un valore errato!!!");
+        controllo = false;
+        Console.Write("\nInserisci UN NUMERO per define il prezzo del prodotto: ");
+    }
+} while (!controllo);
+
+Console.Write("Inserisci la percentuale dell'IVA: ");
+do
+{
+    myString = Console.ReadLine();
+    if (int.TryParse(myString, out iva))
+        if (iva > 0 && iva <= 100)
+        {
+            controllo = true;
+        }
+        else
+        {
+            Console.WriteLine("\nHai inserito un valore errato!!!");
+            controllo = false;
+            Console.Write("\nInserisci UN NUMERO 0 a 100 per definire la percentuale dell'IVA: ");
+        }
+} while (!controllo);
+
+prodotto = new Prodotto(codice, nome, descrizione, prezzo, iva);
+
+
+Console.WriteLine("\n------------------- Prodotto ---------------------\n");
+Console.WriteLine("Codice: \t\t" + $"{prodotto.GetCodice():00000000}");
+Console.WriteLine("Nome: \t\t\t" + prodotto.GetNome());
+Console.WriteLine("Descrizione: \t\t" + prodotto.GetDescrizione());
+Console.WriteLine("Prezzo senza IVA: \t" + $"{prodotto.GetPrezzo():0.##}" + "€");
+Console.WriteLine("IVA: \t\t\t" + prodotto.GetIva() + "%");
+prezzoConIva = prodotto.CalcoloPrezzoConIva(prezzo, iva);
+Console.WriteLine("Prezzo con IVA: \t" + $"{prezzoConIva:0.##}" + "€");
+Console.WriteLine("\n--------------------------------------------------");
+
+
+
+
+
+
+
+void LimitDecimalPlace(string number, int limitPlace)
+{
+    float result = 0;
+    string sNumber = number;
+    int decimalIndex = sNumber.IndexOf(".");
+    Console.WriteLine(decimalIndex);
+    if (decimalIndex != -1)
+    {
+        sNumber = sNumber.Remove(decimalIndex + limitPlace + 1);
+    }
+
+    result = float.Parse(sNumber);
+    result = float.Parse(number);
+    Console.WriteLine(result);
+
+}
